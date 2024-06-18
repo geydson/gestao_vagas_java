a38715b4d6cd0c36f8ad99adffa7a92380e685d4
Geydson Santos <geydson.santos@integralmedica.com.br>
2024-06-18 20:37:55 -0300

    Project Gestão de Vagas

---
+++ src/main/java/br/com/geydsonsantos/gestao_vagas/modules/candidate/useCases/AuthCandidateUseCase.java
@@ -0,0 +1,62 @@
+package br.com.geydsonsantos.gestao_vagas.modules.candidate.useCases;
+
+import java.time.Instant;
+import java.time.Duration;
+
+import java.util.Arrays;
+
+import org.apache.tomcat.websocket.AuthenticationException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+
+import br.com.geydsonsantos.gestao_vagas.modules.candidate.dto.AuthCandidateRequestDTO;
+import br.com.geydsonsantos.gestao_vagas.modules.candidate.dto.AuthCandidateResponseDTO;
+import br.com.geydsonsantos.gestao_vagas.modules.candidate.repositories.CandidateRepository;
+
+@Service
+public class AuthCandidateUseCase {
+
+  @Value("${security.token.secret.candidate}")
+  private String secretKey;
+
+  @Autowired
+  private CandidateRepository candidateRepository;
+
+  @Autowired
+  private PasswordEncoder passwordEncoder;
+
+  public AuthCandidateResponseDTO execute(AuthCandidateRequestDTO authCandidateRequestDTO) throws AuthenticationException {
+    var candidate = this.candidateRepository.findByUsername(authCandidateRequestDTO.username())
+        .orElseThrow(() -> {
+          throw new UsernameNotFoundException("Username/password incorrect!");
+        });
+
+    var passwordMatches = this.passwordEncoder.matches(authCandidateRequestDTO.password(), candidate.getPassword());
+
+    if (!passwordMatches) {
+      throw new AuthenticationException(null);
+    }
+
+    Algorithm algorithm = Algorithm.HMAC256(secretKey);
+    var expiresIn = Instant.now().plus(Duration.ofMinutes(10));
+    var token = JWT.create()
+        .withIssuer("javags")
+        .withSubject(candidate.getId().toString())
+        .withClaim("roles", Arrays.asList("CANDIDATE"))
+        .withExpiresAt(expiresIn)
+        .sign(algorithm);
+
+    var authCandidateResponse = AuthCandidateResponseDTO.builder()
+    .acess_token(token)
+    .expires_in(expiresIn.toEpochMilli())
+    .build();
+
+    return authCandidateResponse;
+  }
+}
---
+++ src/main/java/br/com/geydsonsantos/gestao_vagas/modules/company/dto/AuthCompanyResponseDTO.java
@@ -0,0 +1,17 @@
+package br.com.geydsonsantos.gestao_vagas.modules.company.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthCompanyResponseDTO {
+  
+  private String acess_token;
+  private Long expires_in;
+
+}
